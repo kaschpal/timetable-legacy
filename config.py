36ed8b13d6929fd39036254b588d77cdeba5c859
Ulrich Leutner <leutner@koid.org>
2018-07-18 21:02:07 +0200

    corrected code for locale on windows

--- config.py
+++ config.py
@@ -13,4 +13,13 @@ MAXDATES = 400
 programDirectory = os.path.dirname( os.path.abspath(__file__) ) + os.sep
 stateFile = programDirectory + "state.p"
 dconfPath = "/apps/timetable/"
-language = locale.getdefaultlocale()[0].split("_")[0]
+
+# language
+if os.name == "posix":      # UNIX
+    language = locale.getdefaultlocale()[0].split("_")[0]
+elif os.name == "nt":       # Windows
+    import ctypes
+    windll = ctypes.windll.kernel32
+    language = locale.windows_locale[windll.GetUserDefaultUILanguage()].split("_")[0]
+else:                       # at least try ...
+    language = locale.getdefaultlocale()[0].split("_")[0]
